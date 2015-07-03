5a1f81e01a8cca07d445084764f8f91a2c0f38a2
chen312 <sunshine_chenty@163.com>
2015-07-02 19:46:21 -0700

    Bisection

--- LeetCode/src/leetcode/PowXN.java
+++ LeetCode/src/leetcode/PowXN.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package leetcode;
 
 /**
- *
  * @author Sunshine
  */
 public class PowXN {
+    public static double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        double half = myPow(x, n/2);
+        if (n % 2 == 0)
+            return half * half;
+        else if (n > 0){
+            return half * half * x;
+        }
+        else{
+            return half * half / x;
+        }
+    }
     
+    public static void main(String[] args) {
+        System.out.println(myPow(2, -1));
+    }
 }
