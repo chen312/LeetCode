96c7c3ca4c994b7d040dd4f310cc909a12200a93
chen312 <sunshine_chenty@163.com>
2015-07-25 16:19:30 -0700

    Symmetric Tree

---
+++ LeetCode/src/leetcode/Symmetric_Tree.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * @author Sunshine
+ */
+public class Symmetric_Tree {
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return helper(root.left, root.right);
+    }
+    
+    public static boolean helper(TreeNode left, TreeNode right){
+        if (left == null && right == null)
+            return true;
+        if (left == null || right == null || left.val != right.val)
+            return false;
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
