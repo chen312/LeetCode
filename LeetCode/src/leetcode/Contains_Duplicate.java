8407565765347895fdb5b0cc02bfb31595f54167
chen312 <sunshine_chenty@163.com>
2015-08-05 21:06:06 -0700

    Contains Duplicate

---
+++ LeetCode/src/leetcode/Contains_Duplicate.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.HashSet;
+
+/**
+ * @author Sunshine
+ */
+public class Contains_Duplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++){
+            if (!set.contains(nums[i]))
+                set.add(nums[i]);
+            else
+                return true;
+        }
+        return false;
+    }
+}
